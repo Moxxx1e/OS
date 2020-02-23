2f517fddaff5183008aae39bb51779dd334a499c
Oleg <moxxxie07@gmail.com>
2020-02-23 16:39:32 +0300

    codestyle (tab) fix

--- lab_01/main.c
+++ lab_01/main.c
@@ -16,12 +16,12 @@
 
 int lockfile(int fd)
 {
-    struct flock fl;
-    fl.l_type = F_WRLCK;
-    fl.l_start = 0;
-    fl.l_whence = SEEK_SET;
-    fl.l_len = 0;
-    return(fcntl(fd, F_SETLK, &fl));
+	struct flock fl;
+	fl.l_type = F_WRLCK;
+	fl.l_start = 0;
+	fl.l_whence = SEEK_SET;
+	fl.l_len = 0;
+	return(fcntl(fd, F_SETLK, &fl));
 }
 
 int already_running(void)
